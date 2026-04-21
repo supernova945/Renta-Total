83d8d7791a5aade5f1950f4124ad38be877b034b
supernova945 <92908665+supernova945@users.noreply.github.com>
2026-04-21 14:52:23 -0600

    Subida

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,12 +24,13 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
+    /*
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => 'MIRENTATOTAL',
+        'hostname'     => 'rentatotal-database.postgres.database.azure.com',
+        'username'     => 'Administrador',
+        'password'     => 'ProyectosSv2026!',
+        'database'     => 'Rentatotal',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -51,7 +52,7 @@ class Database extends Config
         ],
 
         
-    ];
+    ];/*
 
     //    /**
     //     * Sample database connection for SQLite3.
@@ -80,27 +81,27 @@ class Database extends Config
     //     *
     //     * @var array<string, mixed>
     //     */
-    //    public array $default = [
-    //        'DSN'        => '',
-    //        'hostname'   => 'localhost',
-    //        'username'   => 'root',
-    //        'password'   => 'root',
-    //        'database'   => 'ci4',
-    //        'schema'     => 'public',
-    //        'DBDriver'   => 'Postgre',
-    //        'DBPrefix'   => '',
-    //        'pConnect'   => false,
-    //        'DBDebug'    => true,
-    //        'charset'    => 'utf8',
-    //        'swapPre'    => '',
-    //        'failover'   => [],
-    //        'port'       => 5432,
-    //        'dateFormat' => [
-    //            'date'     => 'Y-m-d',
-    //            'datetime' => 'Y-m-d H:i:s',
-    //            'time'     => 'H:i:s',
-    //        ],
-    //    ];
+        public array $default = [
+            'DSN'        => '',
+            'hostname'   => 'rentatotal-database.postgres.database.azure.com',
+            'username'   => 'Administrador',
+            'password'   => 'ProyectosSv2026!',
+            'database'   => 'Rentatotal',
+            'schema'     => 'public',
+            'DBDriver'   => 'Postgre',
+            'DBPrefix'   => '',
+            'pConnect'   => false,
+            'DBDebug'    => true,
+            'charset'    => 'utf8',
+            'swapPre'    => '',
+            'failover'   => [],
+            'port'       => 5432,
+            'dateFormat' => [
+               'date'     => 'Y-m-d',
+               'datetime' => 'Y-m-d H:i:s',
+               'time'     => 'H:i:s',
+           ],
+        ];
 
     //    /**
     //     * Sample database connection for SQLSRV.
