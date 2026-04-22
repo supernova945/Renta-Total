0f271636c75ffa9c81093eef230e84ac8cf7b286
supernova945 <92908665+supernova945@users.noreply.github.com>
2026-04-22 17:23:05 -0600

    Revert "Prueba"
    This reverts commit 471c0af1cdebc36c05a081f2635e5cb1b9669584.

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://mirentatotal-evdmgmgggpadanfa.canadacentral-01.azurewebsites.net/index.php';
+    public string $baseURL = 'https://mirentatotal-evdmgmgggpadanfa.canadacentral-01.azurewebsites.net/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
