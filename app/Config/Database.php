cc53bbe03ba3d36d8ce16e891bc3662e67727736
supernova945 <92908665+supernova945@users.noreply.github.com>
2026-04-21 18:10:59 -0600

    Actualizacion

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,6 +24,7 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
+    /*
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
@@ -51,7 +52,7 @@ class Database extends Config
         ],
 
         
-    ];
+    ];*/
 
     //    /**
     //     * Sample database connection for SQLite3.
@@ -75,32 +76,32 @@ class Database extends Config
     //        ],
     //    ];
 
-    //    /**
-    //     * Sample database connection for Postgre.
-    //     *
-    //     * @var array<string, mixed>
-    //     */
-    //    public array $default = [
-    //        'DSN'        => '',
-    //        'hostname'   => 'localhost',
-    //        'username'   => 'root',
-    //        'password'   => 'root',
-    //        'database'   => 'ci4',
-    //        'schema'     => 'public',
-    //        'DBDriver'   => 'Postgre',
-    //        'DBPrefix'   => '',
-    //        'pConnect'   => false,
-    //        'DBDebug'    => true,
-    //        'charset'    => 'utf8',
-    //        'swapPre'    => '',
-    //        'failover'   => [],
-    //        'port'       => 5432,
-    //        'dateFormat' => [
-    //            'date'     => 'Y-m-d',
-    //            'datetime' => 'Y-m-d H:i:s',
-    //            'time'     => 'H:i:s',
-    //        ],
-    //    ];
+    /**
+        * Sample database connection for Postgre.
+        *
+        * @var array<string, mixed>
+        */
+        public array $default = [
+            'DSN'        => '',
+            'hostname'   => 'rentatotal-database.postgres.database.azure.com',
+            'username'   => 'Administrador',
+            'password'   => 'ProyectosSv2026!',
+            'database'   => 'Rentatotal',
+            'schema'     => 'public',
+            'DBDriver'   => 'Postgre',
+            'DBPrefix'   => '',
+            'pConnect'   => false,
+            'DBDebug'    => true,
+            'charset'    => 'utf8',
+            'swapPre'    => '',
+            'failover'   => [],
+            'port'       => 5432,
+            'dateFormat' => [
+                'date'     => 'Y-m-d',
+                'datetime' => 'Y-m-d H:i:s',
+                'time'     => 'H:i:s',
+           ],
+       ];
 
     //    /**
     //     * Sample database connection for SQLSRV.
