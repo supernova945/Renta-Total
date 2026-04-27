0b9a6fb6bb460bb34a9fc9886555ddab793967dc
supernova945 <92908665+supernova945@users.noreply.github.com>
2026-04-27 15:39:53 -0600

    Update ClienteModel.php

--- app/Models/ClienteModel.php
+++ app/Models/ClienteModel.php
@@ -14,7 +14,7 @@ class ClienteModel extends Model
     }
 
     protected $table = 'cliente';
-    protected $primaryKey = 'idCliente';
+    protected $primaryKey = 'idcliente';
 
     protected $useAutoIncrement = true;
 
